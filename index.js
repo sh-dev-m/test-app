28ddbb01da7ae1be87a1cf86215881e181d7089d
Shahriar <shahriyar.m93@gmail.com>
2017-06-02 17:51:20 +0430

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const port=8081;
 var app=express();
 
+app.get('/', (req, res) => {
+  res.status(200).send('Hello, world!');
+});
 
 app.listen(app.get('port'),()=>{
 	console.log('Node app is running on port', app.get('port'));
