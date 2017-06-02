6240dd35d06f3dfa49a299db0486cb84b49a20d0
Shahriar <shahriyar.m93@gmail.com>
2017-06-02 18:21:48 +0430

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
-const port=8081;
+
 var app=express();
+app.set('port', process.env.PORT || 8081);
 
 app.get('/', (req, res) => {
   res.status(200).send('Hello, world!');
